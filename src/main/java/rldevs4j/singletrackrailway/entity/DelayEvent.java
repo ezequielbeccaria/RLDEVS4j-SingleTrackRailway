a5426a9e4a15013a28fb7964f8babcf62f9866e2
Ezequiel <ezequielbeccaria@gmail.com>
2020-05-14 12:15:04 -0300

    Added RandomTimeExogenousEventGen and DelayEvent class

---
+++ src/main/java/rldevs4j/singletrackrailway/entity/DelayEvent.java
@@ -0,0 +1,20 @@
+package rldevs4j.singletrackrailway.entity;
+
+import rldevs4j.base.env.msg.Event;
+import rldevs4j.base.env.msg.EventType;
+
+/**
+ *
+ * @author Ezequiel Beccaria
+ */
+public class DelayEvent extends Event{
+    private int train;
+    private double value;
+
+    public DelayEvent(int id, String name, EventType type, int train, double value) {
+        super(id, name, type);
+        this.train = train;
+        this.value = value;
+    }
+    
+}
